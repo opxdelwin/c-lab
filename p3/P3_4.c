b10d32934f5b56e5bd4bf8af079f3abd9b026697
Delwin Mathew <84124091+opxdelwin@users.noreply.github.com>
2024-02-06 12:47:34 +0530

    Add files via upload

---
+++ p3/P3_4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void) {
+
+	printf("Name                           : Delwin Mathew");
+	printf("Age                              : 18");
+	printf("Gender                         : Male");
+	printf("Qualification                 : HS - SAI Intnl School");
+	printf("Secondary Percentage  : 88.8%");
+	printf("Current Qualification     : BTech 1st Year (CSE)");
+	printf("Goals                            : Batman");
+	printf("Subjects                       : Math, Physics, Chem, BETC, BEE");
+	return 0;
+}
---
+++ p3/P3_5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void) {
+	printf("       *  *  *  *  *  *  *  *  * \n");
+	printf("    *    *                         *\n");
+	printf("  *         *                          *\n");
+	printf("*  *  *  *  *  *  *  *  *  *  *  *  *  *  * \n");
+	printf("*              *                          *\n");
+	printf("*              *                          *\n");
+	printf("*   ____       *                          *\n");
+	printf("*  |    |      *                          *\n");
+	printf("*  |    |      *                          *\n");
+	printf("*  |    |      *                          *\n");
+	printf("*  *  *  *  *  *  *  *  *  *  *  *  *  *  * \n");
+	
+return 0;
+}
